328689c2c3ae35696a5fd4c6810a17fe682d8adc
Pinnapong Silpsakulsuk <pinnapong@gmail.com>
2017-08-05 15:14:04 +0700

    Install koa-bodyparser

--- main.ts
+++ main.ts
@@ -1,4 +1,5 @@
 import * as Koa from 'koa'
+import * as bodyParser from 'koa-bodyparser'
 import {router} from './routes'
 import {Service as ModelService} from './models'
 import {MongoClient, Db} from 'mongodb'
@@ -14,6 +15,7 @@ const main = async () => {
   const app = new Koa()
   app.context.db = await MongoClient.connect('mongodb://localhost:27017/moba')
   app.context.models = new ModelService(app.context.db)
+  app.use(bodyParser())
   app.use(router.routes())
   app.use(router.allowedMethods())
   app.listen(3000)
