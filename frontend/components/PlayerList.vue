f5295abe249915460c8bb637f767689bc5f88b29
Pinnapong Silpsakulsuk <pinnapong@gmail.com>
2017-08-06 18:35:07 +0700

    Add player list component

---
+++ frontend/components/PlayerList.vue
@@ -0,0 +1,25 @@
+<template>
+<div class="table-responsive">
+  <table class="table table-hover">
+    <thead>
+      <th>Name</th>
+      <th>Rating</th>
+    </thead>
+    <tbody>
+      <tr v-for="player in players" :key="player._id">
+        <td>{{player.name}}</td>
+        <td>{{player.rating}}</td>
+      </tr>
+    </tbody>
+  </table>
+</div>
+</template>
+
+<<script>
+export default {
+  props: ['players'],
+  data: function () {
+    return {}
+  }
+}
+</script>
--- frontend/pages/Players.vue
+++ frontend/pages/Players.vue
@@ -1,13 +1,26 @@
 <template>
 <div class="container">
   <h1>Players</h1>
+  <PlayerList :players="players"></PlayerList>
 </div>
 </template>
 
 <script>
+import PlayerList from '../components/PlayerList.vue'
+
 export default {
   data: function () {
-    return {}
+    return {
+      players: [
+        {_id: 'aaa', name: 'Player-1', rating: 1},
+        {_id: 'bbb', name: 'Player-2', rating: 2},
+        {_id: 'ccc', name: 'Player-3', rating: 2},
+        {_id: 'ddd', name: 'Player-4', rating: 2},
+      ]
+    }
   },
+  components: {
+    PlayerList
+  }
 }
 </script>
