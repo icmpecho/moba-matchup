8d1ceb8d29c15563972f57930741bb8f916e7491
Pinnapong Silpsakulsuk <pinnapong@gmail.com>
2017-08-06 16:18:53 +0700

    Change to non-fluid contaienr

--- frontend/App.vue
+++ frontend/App.vue
@@ -1,6 +1,6 @@
 <<template>
   <nav class="navbar navbar-default">
-    <div class="container-fluid">
+    <div class="container">
       <div class="navbar-header">
         <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#nav-menu" aria-expanded="false">
           <span class="icon-bar"></span>
