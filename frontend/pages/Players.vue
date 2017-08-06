61c7c4d4a58af3a5d3282ab4497517933575e25c
Pinnapong Silpsakulsuk <pinnapong@gmail.com>
2017-08-06 19:43:46 +0700

    Fetch players from backend

--- frontend/pages/Players.vue
+++ frontend/pages/Players.vue
@@ -20,6 +20,9 @@ export default {
   },
   components: {
     PlayerList
+  },
+  created: function () {
+    this.$store.dispatch('player/refreshPlayers')
   }
 }
 </script>
--- frontend/store/modules/player.js
+++ frontend/store/modules/player.js
@@ -1,5 +1,24 @@
+import * as $ from 'jquery'
+
 export default {
+  namespaced: true,
   state: {
     players: []
+  },
+  mutations: {
+    refreshPlayers(state, players) {
+      state.players = players
+    }
+  },
+  actions: {
+    refreshPlayers({commit}) {
+      $.get("/api/players").then((data, status) => {
+        if(status == 'success') {
+          commit('refreshPlayers', data)
+        } else {
+          console.log(`[${status}] /api/players`)
+        }
+      })
+    }
   }
 }
