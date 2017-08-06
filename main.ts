2e0c7f1580f457e89f6acfc4df2bfe268f0b3af2
Pinnapong Silpsakulsuk <pinnapong@gmail.com>
2017-08-06 13:56:15 +0700

    Add static middleware to serve files from public dir

--- main.ts
+++ main.ts
@@ -1,4 +1,6 @@
 import * as Koa from 'koa'
+import * as mount from 'koa-mount'
+import * as serve from 'koa-static'
 import * as bodyParser from 'koa-bodyparser'
 import {router} from './routes'
 import {errorHandler, spaHandler} from './middlewares'
@@ -14,6 +16,9 @@ declare module "koa" {
 
 const main = async () => {
   const app = new Koa()
+  const staticFiles = new Koa()
+  staticFiles.use(serve('public'))
+
   const mongoUri = process.env.MONGO_URI || 'mongodb://localhost:27017/moba'
   console.log(`Connecting to the database [ ${mongoUri} ]`)
   app.context.db = await MongoClient.connect(mongoUri)
@@ -25,6 +30,7 @@ const main = async () => {
   app.use(bodyParser())
   app.use(router.routes())
   app.use(router.allowedMethods())
+  app.use(mount('/static', staticFiles))
   app.use(spaHandler)
   console.log("Listening on port 3000.")
   app.listen(3000)
