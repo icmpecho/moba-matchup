386a48b8cbcfbb0f30adac834a79368a490572f3
Pinnapong Silpsakulsuk <pinnapong@gmail.com>
2017-08-06 16:46:20 +0700

    Split nav into component

--- frontend/App.vue
+++ frontend/App.vue
@@ -1,30 +1,17 @@
-<<template>
-  <nav class="navbar navbar-default">
-    <div class="container">
-      <div class="navbar-header">
-        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#nav-menu" aria-expanded="false">
-          <span class="icon-bar"></span>
-          <span class="icon-bar"></span>
-          <span class="icon-bar"></span>
-        </button>
-        <a class="navbar-brand" href="#">MOBA Matchup</a>
-      </div>
-      <div class="collapse navbar-collapse" id="nav-menu">
-        <ul class="nav navbar-nav">
-          <li class="active"><a href="#">Matches</a></li>
-          <li><a href="#">Players</a></li>
-        </ul>
-      </div>
-    </div>
-  </nav>
+<template>
+<Navbar current="players"></Navbar>
 </template>
 
-<<script>
+<script>
+import Navbar from './components/Navbar.vue'
 export default {
   data: function () {
     return {
       greeting: 'Hello'
     }
+  },
+  components: {
+    Navbar
   }
 }
 </script>
---
+++ frontend/components/Navbar.vue
@@ -0,0 +1,41 @@
+<<template>
+  <nav class="navbar navbar-default">
+    <div class="container">
+      <div class="navbar-header">
+        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#nav-menu" aria-expanded="false">
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+        </button>
+        <a class="navbar-brand" href="#">MOBA Matchup</a>
+      </div>
+      <div class="collapse navbar-collapse" id="nav-menu">
+        <ul class="nav navbar-nav">
+          <li :class="gamesBtnClasses"><a href="#">Matches</a></li>
+          <li :class="playersBtnClasses"><a href="#">Players</a></li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<<script>
+export default {
+  props: ['current'],
+  data: function () {
+    return {}
+  },
+  computed: {
+    gamesBtnClasses: function () {
+      return {
+        active: this.current == 'games'
+      }
+    },
+    playersBtnClasses: function () {
+      return {
+        active: this.current == 'players'
+      }
+    },
+  }
+}
+</script>
