7e6c71607e3500465dc356344f74bffda33ac5e1
Pinnapong Silpsakulsuk <pinnapong@gmail.com>
2017-08-06 17:46:09 +0700

    Set landing page to games

--- frontend/router.js
+++ frontend/router.js
@@ -8,6 +8,7 @@ Vue.use(Router)
 export default new Router({
   mode: 'history',
   routes: [
+    { path: '/', redirect: '/games' },
     { path: '/players', component: Players },
     { path: '/games', component: Games },
   ]
