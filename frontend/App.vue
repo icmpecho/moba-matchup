e3299c3a4fbc6d7564346101df087113f421695c
Pinnapong Silpsakulsuk <pinnapong@acommerce.asia>
2017-08-15 15:57:29 +0700

    Add loading spinner

--- frontend/App.vue
+++ frontend/App.vue
@@ -1,5 +1,8 @@
 <template>
   <div id="app">
+    <div class="spin-container" v-if="isLoading">
+      <div class="spinner"></div>
+    </div>
     <Navbar :current="$route.name"></Navbar>
     <router-view></router-view>
   </div>
@@ -10,7 +13,7 @@ import Navbar from './components/Navbar.vue'
 export default {
   data: function () {
     return {
-      greeting: 'Hello'
+      isLoading: false
     }
   },
   components: {
@@ -18,4 +21,40 @@ export default {
   }
 }
 </script>
+
+<style>
+.spin-container {
+  position: fixed;
+  z-index: 998;
+  height: 100%;
+  width: 100%;
+  overflow: show;
+  margin: auto;
+  top: 0;
+  left: 0;
+  bottom: 0;
+  right: 0;
+  background-color: rgba(0,0,0,0.5)
+}
+
+.spinner {
+    border: 8px solid #f3f3f3; /* Light grey */
+    border-top: 8px solid #3498db; /* Blue */
+    border-radius: 50%;
+    width: 60px;
+    height: 60px;
+    animation: spin 2s linear infinite;
+    position: fixed;
+    margin: auto;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+}
+
+@keyframes spin {
+    0% { transform: rotate(0deg); }
+    100% { transform: rotate(360deg); }
+}
+</style>
 
